574005b06a6555b1b1d5116d64121a33dbca119f
Andrew Kim <wk@ualberta.ca>
2019-08-13 15:35:59 +0000

    updated the char counter to display how many more characters are able to be added. Small functionality bug, if a word is copy pasted, the char count doesn't update until the next key is pressed

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  let charRemaining = document.getElementById("text-counter");
+  let maxLength = 140;
+  charRemaining.innerHTML = maxLength;
+  $("#tweet-text").keydown(function() {
+    let currentLength = this.value.length;
+    charRemaining.innerHTML = maxLength - currentLength
+  })
+
+});
