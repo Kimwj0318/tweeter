f2ff8c5924417031e1dacb7be94a81b6aeda3893
Andrew Kim <wk@ualberta.ca>
2019-08-13 21:17:48 +0000

    created a new js file to implement css styling upon hover of tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $("#tweet-text").keyup(function() {
+  $("#tweet-text").keypress(function() {
     let parent = this.closest(".tweet-form");
     let charRemaining = $(this).siblings(".counter")[0];
     const maxLength = 140;
     let currentLength = this.value.length;
-    
+
     charRemaining.innerHTML = maxLength - currentLength
     if(currentLength === maxLength) {
       charRemaining.style.color = "red";
