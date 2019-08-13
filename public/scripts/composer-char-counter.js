0cfbf47ad4d4a7725b8448429bba6d3ad7d7ce59
Andrew Kim <wk@ualberta.ca>
2019-08-13 15:49:46 +0000

    if characters are deleted after the character limit is hit, the character counter turns back to black

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   let charRemaining = document.getElementById("text-counter");
   let maxLength = 140;
   charRemaining.innerHTML = maxLength;
-  $("#tweet-text").keydown(function() {
+  $("#tweet-text").keyup(function() {
     let currentLength = this.value.length;
     charRemaining.innerHTML = maxLength - currentLength
 
     if(currentLength === maxLength) {
       charRemaining.style.color = "red";
+    } else {
+      charRemaining.style.color= "black";
     }
   })
 
